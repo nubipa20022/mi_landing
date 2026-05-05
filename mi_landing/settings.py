402f93c907b4017a9d8f1638f5840a31f855aa2c
nubipa20022 <nubipa20022@gmail.com>
2026-05-05 13:30:54 -0500

    Agrego ruta principal para la landing

--- mi_landing/settings.py
+++ mi_landing/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,10 @@ SECRET_KEY = "django-insecure-vj)#s-=b7bg%a3a+5e)glv0e8+_pi7@pfkhm+je!wgw$*)s3e#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['*']
+
+
 
 
 # Application definition
@@ -116,10 +119,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
-
-import os
-ALLOWED_HOSTS = ['*']
 
 
 STATIC_URL = '/static/'
